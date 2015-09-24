d6f1ed9c2b4695a455e045c66098481cd890a83a
Kevin Dixson <kdixson538@gmail.com>
2015-09-24 16:16:26 -0400

    Clean up OptionsMenu junk
    They’re not used, so bi

--- app/src/main/java/com/dirinc/randomnumbergenerator/EasterEggActivity.java
+++ app/src/main/java/com/dirinc/randomnumbergenerator/EasterEggActivity.java
@@ -2,8 +2,6 @@ package com.dirinc.randomnumbergenerator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
 public class EasterEggActivity extends AppCompatActivity {
 
@@ -13,25 +11,4 @@ public class EasterEggActivity extends AppCompatActivity {
         setContentView(R.layout.activity_easter_egg);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_easter_egg, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
--- app/src/main/java/com/dirinc/randomnumbergenerator/NumberActivity.java
+++ app/src/main/java/com/dirinc/randomnumbergenerator/NumberActivity.java
@@ -1,10 +1,7 @@
 package com.dirinc.randomnumbergenerator;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -14,9 +11,7 @@ import java.util.Random;
 public class NumberActivity extends AppCompatActivity {
 
     public int randomNumber;
-    public int randomNumberInt;
     public int record;
-    public String randomNumberText;
 
     public NumberActivity() {
         record = 1000000;
@@ -28,28 +23,6 @@ public class NumberActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_number);
         generateNumber();
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_number, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
     public void generateNumber() {
