effd7ad8821a375e530bf03e68cf55b3850aee63
Kevin Dixson <kdixson538@gmail.com>
2015-09-23 22:15:04 -0400

    Do it again! button

--- app/src/main/java/com/dirinc/randomnumbergenerator/NumberActivity.java
+++ app/src/main/java/com/dirinc/randomnumbergenerator/NumberActivity.java
@@ -1,9 +1,12 @@
 package com.dirinc.randomnumbergenerator;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.Random;
@@ -64,5 +67,18 @@ public class NumberActivity extends AppCompatActivity {
         setContentView(R.layout.activity_number);
         TextView tv = (TextView) findViewById(R.id.rng);
         if(tv != null) tv.setText(randomNumberText);
+
+        final Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on click
+                emptyMethod();
+            }
+        });
+
+    }
+
+    public void emptyMethod(){
+        generateNumber();
     }
 }
