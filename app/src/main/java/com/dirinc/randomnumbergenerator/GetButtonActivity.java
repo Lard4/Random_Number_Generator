9d369dde22e6ddc065a334ffb3de6a85402f2c29
Kevin Dixson <kdixson538@gmail.com>
2015-10-01 15:23:52 -0400

    Fix Better Buttons [WIP]

--- app/src/main/java/com/dirinc/randomnumbergenerator/GetButtonActivity.java
+++ app/src/main/java/com/dirinc/randomnumbergenerator/GetButtonActivity.java
@@ -7,12 +7,10 @@ import android.widget.Button;
 import android.widget.Toast;
 
 public class GetButtonActivity extends AppCompatActivity {
-    public Button apply1;
     public Boolean isApply1;
 
     public GetButtonActivity() {
         // Initialize the button(s)
-        apply1 = (Button) findViewById(R.id.button1);
         isApply1 = false;
     }
 
@@ -20,15 +18,16 @@ public class GetButtonActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_get_button);
-    }
 
-    public void apply1() {
-        apply1.setOnClickListener(new View.OnClickListener() {
+        final Button better_button_1 = (Button) findViewById(R.id.better_button_1);
+
+        better_button_1.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 isApply1 = true;
                 setButton();
             }
         });
+
     }
 
     public void setButton() {
