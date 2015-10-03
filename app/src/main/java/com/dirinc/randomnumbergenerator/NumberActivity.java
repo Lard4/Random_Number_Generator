2f3ef053116ca59d7aeaa2a4b7f617ea4ea5125a
Kevin Dixson <kdixson538@gmail.com>
2015-10-03 19:16:12 -0400

    Retarded Odds [WIP]

--- app/src/main/java/com/dirinc/randomnumbergenerator/NumberActivity.java
+++ app/src/main/java/com/dirinc/randomnumbergenerator/NumberActivity.java
@@ -12,13 +12,16 @@ import java.util.Random;
 
 public class NumberActivity extends AppCompatActivity {
     public int stashedRecord;
-    public int minimumRandomNumber = 1;
-    public int maximumRandomNumber = 1000000;
+    private int minimumRandomNumber = 1;
+    private int maximumRandomNumber = 1000000;
+    private float odds;
+    private int betterButtonOneCounter;
 
     public static final String SHARED_PREFS = "shared_preferences";
 
     public TextView randomNumber;
     public TextView recordNumber;
+    public TextView percentOdds;
     public Button doItAgain;
 
     public GetButtonActivity Retrieve;
@@ -35,6 +38,7 @@ public class NumberActivity extends AppCompatActivity {
         Retrieve = new GetButtonActivity();
         randomNumber = (TextView) findViewById(R.id.randomNumber);
         recordNumber = (TextView) findViewById(R.id.recordNumber);
+        percentOdds = (TextView) findViewById(R.id.percentOdds);
         doItAgain = (Button) findViewById(R.id.doItAgain);
 
         doItAgain.setOnClickListener(new View.OnClickListener() {
@@ -44,6 +48,7 @@ public class NumberActivity extends AppCompatActivity {
         });
 
         stashedRecord = sharedPreferences.getInt("stashedRecord", maximumRandomNumber);
+        betterButtonOneCounter = sharedPreferences.getInt("stashedBetterButtonOneCounter", 0);
 
         isBetterButton0 = sharedPreferences.getBoolean("better_button0", false);
         isBetterButton1 = sharedPreferences.getBoolean("better_button1", false);
@@ -64,10 +69,22 @@ public class NumberActivity extends AppCompatActivity {
     }
 
     public void decideGeneration() {
+        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS, 0);
         boolean isApply0;
         boolean isApply1;
+        int x;
 
-        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS, 0);
+        if(betterButtonOneCounter > 200) {
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+            editor.putBoolean("better_button1", false);
+            editor.putBoolean("better_button0", true);
+            editor.putInt("stashedBetterButtonOneCounter", 0);
+            editor.commit();
+
+            Toast.makeText(this, "Better Button 1 has ran out of clicks!", Toast.LENGTH_SHORT).show();
+            betterButtonOneCounter = 0;
+        }
+
         isApply0 = sharedPreferences.getBoolean("better_button0", false);
         isApply1 = sharedPreferences.getBoolean("better_button1", false);
 
@@ -105,20 +122,31 @@ public class NumberActivity extends AppCompatActivity {
             recordNumber = (TextView) findViewById(R.id.recordNumber);
             if(recordNumber != null) recordNumber.setText(recordString);
             stashedRecord = randomlyGeneratedNumber;
+            odds = stashedRecord / maximumRandomNumber;
+            makeOdds();
         }
         else {
             recordNumber = (TextView) findViewById(R.id.recordNumber);
             if(recordNumber != null) recordNumber.setText(stashedRecordString);
+            odds = stashedRecord / maximumRandomNumber;
+            makeOdds();
         }
     }
 
     public void generateBetterNumber1() {
+        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS, 0);
+
         int randomlyGeneratedNumber;
         int betterMaximumRandomNumber = 500000;
 
         String recordString;
         String randomlyGeneratedNumberString;
         String stashedRecordString;
+
+        betterButtonOneCounter++;
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putInt("stashedBetterButtonOneCounter", betterButtonOneCounter);
+        editor.commit();
 
         Random rand = new Random();
         randomlyGeneratedNumber = rand.nextInt(betterMaximumRandomNumber - minimumRandomNumber + 1) + minimumRandomNumber;
@@ -132,15 +160,31 @@ public class NumberActivity extends AppCompatActivity {
         randomNumber = (TextView) findViewById(R.id.randomNumber);
         if(randomNumber != null) randomNumber.setText(randomlyGeneratedNumberString);
 
+
         if(randomlyGeneratedNumber < stashedRecord) {
             recordNumber = (TextView) findViewById(R.id.recordNumber);
             if(recordNumber != null) recordNumber.setText(recordString);
             stashedRecord = randomlyGeneratedNumber;
+            odds = randomlyGeneratedNumber / betterMaximumRandomNumber;
+            makeOdds();
         }
         else {
             recordNumber = (TextView) findViewById(R.id.recordNumber);
             if(recordNumber != null) recordNumber.setText(stashedRecordString);
+            odds = stashedRecord / betterMaximumRandomNumber;
+            makeOdds();
         }
+    }
+
+    public void makeOdds() {
+        float myOdds = 10 * 10;
+        String string1 = "You have a total " + myOdds + "% chance";
+        String string2 = "of getting lower than " + stashedRecord;
+        String percentOddsString = string1 + string2;
+
+
+        percentOdds = (TextView) findViewById(R.id.percentOdds);
+        if(percentOdds != null) percentOdds.setText(percentOddsString);
     }
 
     @Override
@@ -148,6 +192,7 @@ public class NumberActivity extends AppCompatActivity {
         SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS, 0);
         SharedPreferences.Editor editor = sharedPreferences.edit();
         editor.putInt("stashedRecord", stashedRecord);
+        editor.putInt("stashedBetterButtonOneCounter", betterButtonOneCounter);
         editor.commit();
 
         super.onStop();
