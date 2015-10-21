bd8c88e0057b1f8487f2793808fa0d47f86e1268
Kevin Dixson <kdixson538@gmail.com>
2015-10-21 17:53:09 -0400

    Achievements, leaderboards, and logs, OH MY

--- app/src/main/java/com/dirinc/randomnumbergenerator/MainActivity.java
+++ app/src/main/java/com/dirinc/randomnumbergenerator/MainActivity.java
@@ -8,11 +8,11 @@ import android.view.HapticFeedbackConstants;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+import android.util.Log;
 
 import com.google.android.gms.common.*;
 import com.google.android.gms.common.api.*;
 import com.google.android.gms.games.*;
-import com.google.android.gms.plus.*;
 import com.google.example.games.basegameutils.*;
 
 public class MainActivity extends Activity
@@ -20,7 +20,7 @@ public class MainActivity extends Activity
         View.OnClickListener, View.OnLongClickListener {
 
     private static final String SHARED_PREFS = "shared_preferences";
-    private Button number_button, button_more;
+
     private GoogleApiClient mGoogleApiClient;
     private static int RC_SIGN_IN = 9001;
 
@@ -37,20 +37,18 @@ public class MainActivity extends Activity
         mGoogleApiClient = new GoogleApiClient.Builder(this)
                 .addConnectionCallbacks(this)
                 .addOnConnectionFailedListener(this)
-                .addApi(Plus.API).addScope(Plus.SCOPE_PLUS_LOGIN)
                 .addApi(Games.API).addScope(Games.SCOPE_GAMES)
                 .build();
-
         signIn();
         initializeButtons();
     }
 
     public void initializeButtons() {
-        number_button = (Button) findViewById(R.id.number_button);
+        Button number_button = (Button) findViewById(R.id.number_button);
         number_button.setOnClickListener(this);
         number_button.setOnLongClickListener(this);
 
-        button_more = (Button) findViewById(R.id.button_more);
+        Button button_more = (Button) findViewById(R.id.button_more);
         button_more.setOnClickListener(this);
     }
 
@@ -156,34 +154,27 @@ public class MainActivity extends Activity
 
     @Override
     public void onConnected(Bundle bundle) {
-        Toast.makeText(getApplicationContext(), "Connected to Google Play", Toast.LENGTH_SHORT).show();
+        Log.d("GPS", "Google Play Games CONNECTED");
     }
 
     @Override
     public void onConnectionSuspended(int i) {
-        Toast.makeText(getApplicationContext(), "Connection to Google Play suspended", Toast.LENGTH_SHORT).show();
+        Log.d("GPS", "Google Play Games SUSPENDED");
         mGoogleApiClient.connect();
     }
 
     @Override
     public void onConnectionFailed(ConnectionResult connectionResult) {
-        Toast.makeText(getApplicationContext(), "Connection to Google Play failed!", Toast.LENGTH_SHORT).show();
+        Log.d("GPS", "Google Play Games FAILED");
         if (mResolvingConnectionFailure) {
-            // already resolving
             return;
         }
 
-        // if the sign-in button was clicked or if auto sign-in is enabled,
-        // launch the sign-in flow
         if (mSignInClicked || mAutoStartSignInFlow) {
             mAutoStartSignInFlow = false;
             mSignInClicked = false;
             mResolvingConnectionFailure = true;
 
-            // Attempt to resolve the connection failure using BaseGameUtils.
-            // The R.string.signin_other_error value should reference a generic
-            // error string in your strings.xml file, such as "There was
-            // an issue with sign-in, please try again later."
             if (!BaseGameUtils.resolveConnectionFailure(this,
                     mGoogleApiClient, connectionResult,
                     RC_SIGN_IN, "Connection to Google Play failed!")) {
@@ -210,5 +201,4 @@ public class MainActivity extends Activity
             }
         }
     }
-
 }
--- app/src/main/java/com/dirinc/randomnumbergenerator/NumberActivity.java
+++ app/src/main/java/com/dirinc/randomnumbergenerator/NumberActivity.java
@@ -1,32 +1,63 @@
 package com.dirinc.randomnumbergenerator;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.HapticFeedbackConstants;
 import android.widget.Button;
 import android.widget.TextView;
 import android.view.View;
 import android.widget.Toast;
 
+import com.google.android.gms.common.*;
+import com.google.android.gms.common.api.*;
+import com.google.android.gms.games.*;
+import com.google.example.games.basegameutils.BaseGameUtils;
+
 import java.util.Random;
 
-public class NumberActivity extends AppCompatActivity {
+public class NumberActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener {
+
     private int stashedRecord;
     private int minimumRandomNumber = 1;
     private int betterButtonOneCounter;
+    private boolean firstTimePurpley = true;
+    private boolean firstTimeYellow = true;
+
     private String unlockedColor;
+
     private TextView randomNumber;
     private TextView recordNumber;
     private TextView percentOdds;
-    private Button doItAgain;
+
     private static final String SHARED_PREFS = "shared_preferences";
+
+    // Google Play API stuff
+    private GoogleApiClient mGoogleApiClient;
+
+    private boolean mResolvingConnectionFailure = false;
+    private boolean mSignInClicked = false;
+    private boolean mAutoStartSignInFlow = true;
+
+    // Random ass number
+    private static final int RC_UNUSED = 5001;
+    private static final int RC_SIGN_IN = 9001;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setColors();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_number);
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(Games.API).addScope(Games.SCOPE_GAMES)
+                .build();
+        signIn();
 
         startNumberActivity();
     }
@@ -40,7 +71,7 @@ public class NumberActivity extends AppCompatActivity {
         randomNumber = (TextView) findViewById(R.id.randomNumber);
         recordNumber = (TextView) findViewById(R.id.recordNumber);
         percentOdds = (TextView) findViewById(R.id.percentOdds);
-        doItAgain = (Button) findViewById(R.id.doItAgain);
+        Button doItAgain = (Button) findViewById(R.id.doItAgain);
 
         doItAgain.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -116,11 +147,13 @@ public class NumberActivity extends AppCompatActivity {
         if(randomNumber != null) randomNumber.setText(randomlyGeneratedNumberString);
 
         if(randomlyGeneratedNumber < stashedRecord) {
-            setRecord(randomlyGeneratedNumber);
-            makeOdds(randomlyGeneratedNumber, maximumRandomNumber);
+            stashedRecord = randomlyGeneratedNumber;
+            // Zero for standard button
+            setRecord(stashedRecord, 0);
         } else {
             refreshRecord();
         }
+        makeOdds(stashedRecord, maximumRandomNumber);
     }
 
     public void generateBetterNumber1() {
@@ -137,11 +170,14 @@ public class NumberActivity extends AppCompatActivity {
         if(randomNumber != null) randomNumber.setText(randomlyGeneratedNumberString);
 
         if(randomlyGeneratedNumber < stashedRecord) {
-            setRecord(randomlyGeneratedNumber);
-            makeOdds(randomlyGeneratedNumber, maximumRandomNumber);
+            stashedRecord = randomlyGeneratedNumber;
+            // One for better button
+            setRecord(stashedRecord, 1);
         } else {
             refreshRecord();
         }
+        makeOdds(stashedRecord, maximumRandomNumber);
+
 
         betterButtonOneCounter++;
         SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS, 0);
@@ -198,16 +234,17 @@ public class NumberActivity extends AppCompatActivity {
         if (percentOdds != null) percentOdds.setText(percentOddsString);
     }
 
-    public void setRecord(int r) { // IF FROM GENERATENUMBER__()
+    public void setRecord(int r, int b) { // IF FROM GENERATENUMBER__()
         String recordString = "Record: " + r;
 
         recordNumber = (TextView) findViewById(R.id.recordNumber);
         if(recordNumber != null) recordNumber.setText(recordString);
-        stashedRecord = r;
 
         saveInfo();
-            // TODO: Save google play games leader board
+        if(stashedRecord < 10000) updateLeaderboard(b);
         decideColor();
+        if((unlockedColor.equals("Purple") && (firstTimePurpley))) achievementsPurpley();
+        if((unlockedColor.equals("Yellow") && (firstTimeYellow))) achievementsHalfway();
     }
 
     public void refreshRecord() { // ELSE FROM GENERATENUMBER__()
@@ -246,6 +283,111 @@ public class NumberActivity extends AppCompatActivity {
             default:
                 setTheme(R.style.AppTheme);
                 break;
+        }
+    }
+
+    public void updateLeaderboard(int b) {
+        Log.d("FUNCTIONALITY", "Updating leaderboard");
+        if (isSignedIn()) {
+            if (b == 0) {
+                Log.d("LEADERBOARD", "Updating leaderboard: Standard Button");
+                Games.Leaderboards.submitScore(mGoogleApiClient, getString(R.string.leaderboard_STANDARDBUTTON), stashedRecord);
+                startActivityForResult(Games.Leaderboards.getLeaderboardIntent(mGoogleApiClient,
+                        getString(R.string.leaderboard_STANDARDBUTTON)), RC_UNUSED);
+            } else if (b == 1) {
+                Log.d("LEADERBOARD", "Updating leaderboard: Better Button");
+                Games.Leaderboards.submitScore(mGoogleApiClient, getString(R.string.leaderboard_BETTERBUTTON), stashedRecord);
+                startActivityForResult(Games.Leaderboards.getLeaderboardIntent(mGoogleApiClient,
+                        getString(R.string.leaderboard_BETTERBUTTON)), RC_UNUSED);
+            }
+        } else {
+            BaseGameUtils.makeSimpleDialog(this, "Google Play Services failed to connect").show();
+            while (!isSignedIn()) {
+                Log.d("GPS", "In loop of not signed in! Waiting for connection.");
+            }
+            updateLeaderboard(b);
+        }
+    }
+
+    public void achievementsPurpley() {
+        Log.d("FUNCTIONALITY", "Updating Achievement: Purpley");
+        firstTimePurpley = false;
+        if (isSignedIn()) {
+            Games.Achievements.unlock(mGoogleApiClient, getString(R.string.achievement_PURPLEY));
+            startActivityForResult(Games.Achievements.getAchievementsIntent(mGoogleApiClient),
+                    RC_UNUSED);
+        } else {
+            BaseGameUtils.makeSimpleDialog(this, "Google Play Services failed to connect").show();
+            while (!isSignedIn()) {
+                Log.d("GPS", "In loop of not signed in! Waiting for connection.");
+            }
+            achievementsPurpley();
+        }
+    }
+
+    public void achievementsHalfway() {
+        Log.d("FUNCTIONALITY", "Updating Achievement: Halfway");
+        firstTimeYellow = false;
+        if (isSignedIn()) {
+            Games.Achievements.unlock(mGoogleApiClient, getString(R.string.achievement_HALFWAYTHERE));
+            startActivityForResult(Games.Achievements.getAchievementsIntent(mGoogleApiClient),
+                    RC_UNUSED);
+        } else {
+            BaseGameUtils.makeSimpleDialog(this, "Google Play Services failed to connect").show();
+            while (!isSignedIn()) {
+                Log.d("GPS", "In loop of not signed in! Waiting for connection.");
+            }
+            achievementsHalfway();
+        }
+    }
+
+    private void signIn() {
+        mSignInClicked = true;
+        mGoogleApiClient.connect();
+    }
+
+    private boolean isSignedIn() {
+        return (mGoogleApiClient != null && mGoogleApiClient.isConnected());
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        Log.d("GPS", "Google Play Games CONNECTED");
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.d("GPS", "Google Play Games SUSPENDED");
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        Log.d("GPS", "Google Play Games FAILED");
+        if (mResolvingConnectionFailure) {
+            return;
+        }
+
+        if (mSignInClicked || mAutoStartSignInFlow) {
+            mAutoStartSignInFlow = false;
+            mSignInClicked = false;
+
+            mResolvingConnectionFailure = BaseGameUtils.resolveConnectionFailure(this,
+                    mGoogleApiClient, connectionResult,
+                    RC_SIGN_IN, "Connection to Google Play failed!");
+        }
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        if (requestCode == RC_SIGN_IN) {
+            mSignInClicked = false;
+            mResolvingConnectionFailure = false;
+            if (resultCode == RESULT_OK) {
+                mGoogleApiClient.connect();
+            } else {
+                BaseGameUtils.showActivityResultError(this,
+                        requestCode, resultCode, 0);
+            }
         }
     }
 
